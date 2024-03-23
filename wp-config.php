333f030362afd2976f6ad5ca1fa04b47ac00c7d8
Pranit Dugad <pranit.dugad@multidots.com>
2024-03-23 12:28:32 +0530

    my portfolio project

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'root' );
+define( 'DB_NAME', '150572843' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'pranitdugad' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'Pranit@7721851108' );
 
 /** Database hostname */
 define( 'DB_HOST', 'https://portfolio-pranit.wpcomstaging.com' );
