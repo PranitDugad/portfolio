c9d2f9beebb63a700e8f58254fcdde755b703b2f
Pranit Dugad <pranit.dugad@multidots.com>
2024-03-23 12:19:04 +0530

    my portfolio project

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'https://portfolio-pranit.wpcomstaging.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
